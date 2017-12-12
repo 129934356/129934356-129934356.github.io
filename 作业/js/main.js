8786171274629487cff198b9607c4cf7829c23e6
129934356 <1299343506@qq.com>
2017-12-12 19:51:08 +0800

    xintianjia

---
+++ 作业/js/main.js
@@ -0,0 +1,39 @@
+var mySwiper = new Swiper ('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    on:{
+        init: function(){
+            swiperAnimateCache(this);
+            swiperAnimate(this);
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this);
+        }
+    }
+})
+
+var music=document.querySelector('.music');
+var music_off=document.querySelector('.music_off');
+var audio=document.querySelector('audio');
+var flag=1;
+music.onclick=function (){
+    if(flag==1){
+        music.style.animation="none";
+        music_off.style.display="none";
+        audio.pause();
+        flag=0;
+    }
+    else{
+        music.style.animation="cube 3s infinite linear";
+        music_off.style.display="block";
+        audio.play();
+        flag=1;
+    }
+}
